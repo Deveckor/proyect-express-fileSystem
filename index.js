a1242eeffbe1d3535801fb7966fa968772253543
deveckor <deveckor@gmail.com>
2022-02-18 13:25:24 -0600

    modify script

--- index.js
+++ index.js
@@ -6,18 +6,16 @@ const port = 8080;
 const listJson = fs.readFileSync('./koders.json');
 const koders = JSON.parse(listJson);
 
-koders.koders.forEach(el =>{
-    console.log(el.name);
-})
+
 
 app.use(express.json());
 
 app
     .get('/koders',(req,res) => {
         
-            koders.koders.forEach(el=>{
-                res.write(JSON.stringify(el.name));
-            })
+           
+                res.write(JSON.stringify(koders));
+           
             
         res.end();
     })
