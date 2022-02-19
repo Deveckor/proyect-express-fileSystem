0f203d593c6f31e4bcab1d8b1e8843519b8cd0e5
deveckor <deveckor@gmail.com>
2022-02-18 19:33:32 -0600

    add script

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const fs = require('fs');
 const app = express();
 const port = 8080;
 
-const listJson = fs.readFileSync('./koders.json');
+const listJson = fs.readFileSync('./koders.json','utf-8');
+const koders = JSON.parse(listJson);
 
 
 
+app.use(express.json());
 
 
 
@@ -14,11 +16,34 @@ app
     .get('/koders',(req,res) => {
         
            
-                res.write(listJson);
+               res.json(koders);
            
             
-        res.end();
+        
+    })
+    .post('/koders',(req,res) => {
+        const body = req.body;
+        
+        
+        fs.readFile('./koders.json',(err, data) => {
+            if (err) {
+                return console.log(err);
+            }
+            let koders = JSON.parse(data);
+            let koder = koders.koders
+            koder.push(body);
+            
+            
+            fs.writeFile('./koders.json',JSON.stringify(koders), (err) =>{
+                if (err) {
+                    return console.log(err)
+                }
+            })
+        })
+        res.end()
     })
     .listen(port, ()=>{
         console.log(`Servidor escuchando en el puerto ${port}`);
     })
+
+
